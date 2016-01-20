b3f568f7ea751c1e702a492d6ec973731fcb56d8
andrewgrewell <andrew.grewell@odysseyscience.com>
2016-01-19 22:15:33 -0800

    trying to find entry for prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
     devtool: 'sourcemaps',
     entry: [
-        './app/entry.js'
+        'app/entry.js'
     ],
     output: {
         path: path.join(__dirname, 'server/public/js/'),
